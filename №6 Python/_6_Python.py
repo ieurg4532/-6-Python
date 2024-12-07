ee13c7c2fe34c08c7b91f1fd78fa4dd358a345e0
Matrosov <Matrosov@KOKSFENMETT>
2024-12-07 23:28:44 +0100

    Добавьте файлы проекта.

---
+++ №6 Python/_6_Python.py
@@ -0,0 +1,13 @@
+﻿
+def delete(): 
+    a = list(map(int, input('Enter your numbers: ').split()))
+    print("Начальний список: ", a)
+
+    result = []
+
+    for i in range(len(a)): 
+        if (i+1) % 2 == 0: 
+            result.append(a[i])
+    print("Кінцевий список: ", result)
+delete()
+
